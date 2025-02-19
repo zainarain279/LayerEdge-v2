d81c026dcb0385eef609c2e86764f4ce23a677e5
Muhammad Zain <zainarain2024@gmail.com>
2025-02-19 13:54:19 +0500

    Create banner.js

---
+++ utils/banner.js
@@ -0,0 +1,18 @@
+export default `\n
+░▀▀█░█▀█░▀█▀░█▀█
+░▄▀░░█▀█░░█░░█░█
+░▀▀▀░▀░▀░▀▀▀░▀░▀
+╔══════════════════════════════════╗
+║                                  ║
+║  ZAIN ARAIN                      ║
+║  AUTO SCRIPT MASTER              ║
+║                                  ║
+║  JOIN TELEGRAM CHANNEL NOW!      ║
+║  https://t.me/AirdropScript6              ║
+║  @AirdropScript6 - OFFICIAL      ║
+║  CHANNEL                         ║
+║                                  ║
+║  FAST - RELIABLE - SECURE        ║
+║  SCRIPTS EXPERT                  ║
+║                                  ║
+╚══════════════════════════════════╝ `;
